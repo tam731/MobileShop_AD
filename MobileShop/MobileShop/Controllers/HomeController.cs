f71293862cda5d8cbe97f56dfc8d7d800cb6f79f
tam731 <levantam4199@gmail.com>
2021-04-13 01:37:05 +0700

    update login and logout

--- MobileShop/MobileShop/Controllers/HomeController.cs
+++ MobileShop/MobileShop/Controllers/HomeController.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using CaptchaMvc.HtmlHelpers;
+using MobileShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,9 +13,89 @@ namespace MobileShop.Controllers
     public class HomeController : Controller
     {
         // GET: Home
+        ShopMobileManagementEntities db = new ShopMobileManagementEntities();
         public ActionResult Index()
         {
             return View();
         }
+        //login
+        [HttpGet]
+        public ActionResult AccountLogin()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AccountLogin(FormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                string userName = collection["txtusername"].ToString();
+                string password = collection["txtpassword"].ToString();
+
+                //var f_password = GetMD5(password);
+                var data = db.Users.Where(s => s.UserName.Equals(userName) && s.Password.Equals(password)).ToList();
+                //if (u != null)
+                //{
+                //    Session["UserName"] = u;
+                //    return RedirectToAction("Index");
+                //}
+                if (data.Count() > 0)
+                {
+                    //add session
+                    Session["FullName"] = data.FirstOrDefault().FullName;
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.error = "Login failed";
+                    return RedirectToAction("AccountLogin");
+                }
+            }
+            return View();
+            
+        }
+        public ActionResult Logout()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("Index");
+        }
+
+        //register
+        [HttpGet]
+        public ActionResult Regiter()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Regiter(User user, FormCollection collection)
+        {
+            if (this.IsCaptchaValid("Captcha is not valid"))
+            {
+                ViewBag.mesage = "Successfully";
+                db.Users.Add(user);
+                db.SaveChanges();
+                return View();
+            }
+            ViewBag.mesage = "Falied";
+            return View();
+        }
+
+        //Password encryption
+        public static string GetMD5(string str)
+        {
+            MD5 md5 = new MD5CryptoServiceProvider();
+            byte[] fromData = Encoding.UTF8.GetBytes(str);
+            byte[] targetData = md5.ComputeHash(fromData);
+            string byte2String = null;
+
+            for (int i = 0; i < targetData.Length; i++)
+            {
+                byte2String += targetData[i].ToString("x2");
+
+            }
+            return byte2String;
+        }
+
     }
 }
--- MobileShop/MobileShop/Controllers/UserController.cs
+++ MobileShop/MobileShop/Controllers/UserController.cs
@@ -28,34 +28,7 @@ namespace MobileShop.Controllers
             User u = db.Users.Where(x => x.ID == 2).FirstOrDefault();
             return View(u);
         }
-        public ActionResult UserSort()
-        {
-            List<User> user =db.Users.OrderBy(x=>x.FullName).ToList();
-            return View(user);
-        }
-        public ActionResult UserGroup()
-        {
-            List<User> user = db.Users.OrderBy(x => x.FullName).ToList();
-            return View(user);
-        }
-        //register
-        [HttpGet]
-        public ActionResult Account()
-        {
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Account(User user,FormCollection collection,string check)
-        {
-            if(this.IsCaptchaValid("Captcha is not valid"))
-            {
-                ViewBag.mesage = "Successfully";
-                db.Users.Add(user);
-                db.SaveChanges();
-                return View();
-            }
-            ViewBag.mesage = "Falied";
-            return View();
-        }
+       
+       
     }
 }
